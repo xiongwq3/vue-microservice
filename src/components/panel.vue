7f260f14388efcfae6a8be812d8bc4dc324e77f6
Jiahonzheng <jiahonzheng@gmail.com>
2020-10-26 02:16:51 +0800

    feat: 添加 Grafana Panel 组件

---
+++ src/components/panel.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-card>
+    <iframe
+      src="http://139.9.57.167:3000/d-solo/82IXLA5Mk/test-dashboard?orgId=1&panelId=2"
+      width="600"
+      height="400"
+      frameborder="0"
+    />
+  </el-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ src/views/Monitor.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-container class="monitor-container">
+    <el-row :gutter="20" justify="space-around">
+      <el-col :span="12">
+        <panel />
+      </el-col>
+      <el-col :span="12">
+        <panel />
+      </el-col>
+    </el-row>
+    <el-row :gutter="20" justify="space-around" class="monitor-row">
+      <el-col :span="12">
+        <panel />
+      </el-col>
+      <el-col :span="12">
+        <panel />
+      </el-col>
+    </el-row>
+  </el-container>
+</template>
+
+<script>
+import Panel from "@/components/panel";
+
+export default {
+  components: { Panel }
+};
+</script>
+
+<style lang="scss">
+.monitor-container {
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.monitor-row {
+  margin-top: 20px;
+}
+</style>
